cb75bc1e6fc49dd796a54685a38ce8ad5a1a31b4
ardhikaxx <ardhikayanuar58@gmail.com>
2026-02-08 14:37:06 +0700

    fix(edukasi): perbaiki tampilan tombol aksi dengan warna solid dan icon di tengah
    - Ubah tombol soft colors menjadi warna solid (primary, danger)
    - Icon berada di tengah tombol dengan flexbox
    - Ukuran tombol 36px x 36px
    - Icon menggunakan warna putih untuk kontras

--- resources/views/edukasi/index.blade.php
+++ resources/views/edukasi/index.blade.php
@@ -77,12 +77,18 @@
                                             @endif
                                         </td>
                                         <td class="text-center">
-                                            <div class="d-flex justify-content-center gap-2">
-                                                <a class="btn btn-soft-primary btn-sm icon-btn rounded-3" href="{{ route('edukasi.edit', $item->id_edukasi) }}" title="Edit">
-                                                    <i class="mdi mdi-pencil-outline"></i>
+                                            <div class="action-buttons d-flex justify-content-center gap-2">
+                                                <a class="btn btn-primary btn-sm rounded-3 d-flex align-items-center justify-content-center"
+                                                    href="{{ route('edukasi.edit', $item->id_edukasi) }}"
+                                                    title="Edit"
+                                                    style="width: 36px; height: 36px;">
+                                                    <i class="mdi mdi-pencil text-white"></i>
                                                 </a>
-                                                <button class="btn btn-soft-danger btn-sm icon-btn rounded-3" onclick="deleteConfirmation('{{ route('edukasi.hapus', $item->id_edukasi) }}')" title="Hapus">
-                                                    <i class="mdi mdi-delete-outline"></i>
+                                                <button class="btn btn-danger btn-sm rounded-3 d-flex align-items-center justify-content-center"
+                                                    onclick="deleteConfirmation('{{ route('edukasi.hapus', $item->id_edukasi) }}')"
+                                                    title="Hapus"
+                                                    style="width: 36px; height: 36px;">
+                                                    <i class="mdi mdi-trash-can text-white"></i>
                                                 </button>
                                             </div>
                                         </td>
