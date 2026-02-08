5698b532067cbd494761c1cb81297cb386376766
ardhikaxx <ardhikayanuar58@gmail.com>
2026-02-08 12:42:28 +0700

    update(cdn): gunakan CDN Bootstrap 5.3.3 terbaru dengan integrity hash

--- resources/views/layouts/template.blade.php
+++ resources/views/layouts/template.blade.php
@@ -15,6 +15,8 @@
     <link rel="shortcut icon" href="{{ asset('assets/images/logo-mini.png') }}" />
     <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/sweetalert2@11.0.19/dist/sweetalert2.min.css">
     <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap-datepicker@1.9.0/dist/css/bootstrap-datepicker.min.css">
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-QWTKZyjpPEjISv5WaRU9OFeRpok6YctnYmDr5pNlyT2bRjXh0JMhjY6hW+ALEwIH" crossorigin="anonymous">
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/js/bootstrap.bundle.min.js" integrity="sha384-YvpcrYf0tY3lHB60NNkmXc5s9fDVZLESaAA55NDzOxhy9GkcIdslK1eN7N6jIeHz" crossorigin="anonymous"></script>
     <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/5.15.3/css/all.min.css">
     <!-- Memasukkan jQuery -->
 <script src="https://code.jquery.com/jquery-3.6.0.min.js"></script>
